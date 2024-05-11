db04ecca7cc1e81a96d74a81f2a3e87db7aaa417
Tushar Pawar <tushharpawar14@gmail.com>
2024-05-11 21:51:45 +0530

    Create route.tsx

---
+++ next_auth/src/app/api/users/signup/route.tsx
@@ -0,0 +1,48 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { error } from "console";
+import { NextRequest,NextResponse} from 'next/server';
+import bcryptjs from 'bcryptjs'
+import {sendMail} from "@/helpers/mailer"
+
+connect();
+
+export async function POST(request:NextRequest ) {
+        try {
+
+            const reqBody = await request.json()
+            const {username,email,password}  = reqBody;
+
+            console.log(reqBody);
+            const user= await User.findOne({email})
+
+            if(user) {
+                return NextResponse.json({error:"User already exists"},{status:400})
+            }
+
+            const salt = await bcryptjs.genSalt(10);
+
+            const hashedPassword = await bcryptjs.hash(password,salt);
+
+            const newUser = new User({
+                username,
+                email,
+                password:hashedPassword
+            })
+            
+            const savedUser=await newUser.save()
+
+            console.log(savedUser);
+
+            //send varification email
+            await sendMail({email,emailType:"VERIFY",userId:savedUser._id})
+
+            return NextResponse.json({
+                message:"User registered successfully!!!!",
+                success:true,
+                savedUser
+            })
+        } catch (error : any) {
+            return NextResponse.json({error : error.message },{status:500})
+        }
+}
