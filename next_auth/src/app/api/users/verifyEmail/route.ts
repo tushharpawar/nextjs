92def345cb65a825b1eb61302f3ea7ae3f96412e
Tushar Pawar <tushharpawar14@gmail.com>
2024-05-13 20:39:15 +0530

    Create route.ts

---
+++ next_auth/src/app/api/users/verifyEmail/route.ts
@@ -0,0 +1,31 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { error } from "console";
+import { NextRequest,NextResponse} from 'next/server';
+
+connect();
+
+export async function POST (req:NextRequest){
+    try {
+            const reqBody = await req.json()
+            const{token} = reqBody;
+            console.log(token);
+
+            const user = await User.findOne({verifyToken : token , verifyTokenExpiry : { $gt : Date.now()}});
+
+            if(!user) {
+                return NextResponse.json({error:"Invalid Token"},{status:400})
+            }
+            console.log(user);
+
+            user.isVerified = true;
+            user.verifyToken = undefined;
+            user.verifyTokenExpiry = undefined;
+            
+            await user.save();
+            
+            return NextResponse.json({message:"Email verified successfully",success :true},{status:200})
+    } catch (error:any) {
+        return NextResponse.json({error:error.message},{status:500})
+    }
+}
