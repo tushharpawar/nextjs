fb1311f6e6dc54fe8272cca559d8d1d0ec20e5f1
Tushar Pawar <tushharpawar14@gmail.com>
2024-05-14 21:36:46 +0530

    Create messageSchema.ts

---
+++ mstrymessage/src/schemas/messageSchema.ts
@@ -0,0 +1,6 @@
+import {z} from 'zod'
+
+export const messageSchema = z.object({
+    content:z.string().min(10,{message:"Content must be 10 characters"})
+    .max(300,{message:"Content must be les than 300 characters"}),
+}) 
