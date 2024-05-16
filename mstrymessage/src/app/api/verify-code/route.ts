1068553be51cff8cde0dd98369c1479e80170d2c
Tushar Pawar <tushharpawar14@gmail.com>
2024-05-16 21:27:29 +0530

    Create route.ts

---
+++ mstrymessage/src/app/api/verify-code/route.ts
@@ -0,0 +1,55 @@
+import dbConnect from '@/lib/dbConnect'
+import UserModel from '@/model/User'
+import { Elsie } from 'next/font/google';
+import { use } from 'react';
+
+export async function POST(req:Request) {
+    await dbConnect();
+
+    try {
+        const {username , code } = await req.json()
+
+        const decodedUsername = decodeURIComponent(username)
+        const user = await UserModel.findOne({username :decodedUsername})
+
+        if(!user){
+            return Response.json({
+                success:false,
+                message: "User not found"
+            },{status:500})
+        }
+
+        const isCodeValid = user.verifyCode === code
+        const isCodeExpired = new Date(user.verifyCodeExpiry) > new Date()
+
+        if(isCodeValid && isCodeExpired){
+            user.isVerified =true
+            await user.save()
+
+            return Response.json({
+                success:true,
+                message: "User verified successfully!"
+            },{status:200})
+        }
+        else if(!isCodeExpired){
+            return Response.json({
+                success:false,
+                message: "Verification code is expired please signup again"
+            },{status:500})
+        }
+        else{
+            return Response.json({
+                success:false,
+                message: "Incorrect verification code"
+            },{status:500})
+        }
+
+    } catch (error) {
+        console.error("Error verifying user.",error)
+
+            return Response.json({
+                success:false,
+                message:"Error verifying user"
+            },{status:500})
+    }
+}
