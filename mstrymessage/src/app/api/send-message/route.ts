65037a8f5b3e72accf87b4533bae89a0c9cb9323
Tushar Pawar <tushharpawar14@gmail.com>
2024-05-18 22:42:55 +0530

    Create route.ts

---
+++ mstrymessage/src/app/api/send-message/route.ts
@@ -0,0 +1,55 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { Message } from "@/model/User";
+
+export async function POST(req: Request) {
+  await dbConnect();
+
+  const { username, content } = await req.json();
+
+  try {
+    const user = await UserModel.findOne({ username });
+
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 404 }
+      );
+    }
+
+    // is user accepting the messages or not
+
+    if(!user.isAcceptingMessage){
+        return Response.json(
+            {
+              success: false,
+              message: "User is not accepting messages",
+            },
+            { status: 403 }
+          );
+    }
+
+    const newMessage = {content,createdAt: new Date()} 
+
+    user.messages.push(newMessage as Message)
+    await user.save()
+
+    return Response.json(
+        {
+          success: true,
+          message: "Message sent successfully",
+        },
+        { status: 200 }
+      );
+
+  } catch (error) {
+    console.error('Error adding message:', error);
+    return Response.json(
+      { message: 'Internal server error', success: false },
+      { status: 500 }
+    );
+  }
+}
