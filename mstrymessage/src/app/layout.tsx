ab41603292aa1423f7b47be8daca853f5b6c4c00
Tushar Pawar <tushharpawar14@gmail.com>
2024-05-20 20:35:01 +0530

    Update layout.tsx

--- mstrymessage/src/app/layout.tsx
+++ mstrymessage/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster";
+import AuthProvider from "@/context/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,10 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-      {children}
-      <Toaster />
-      </body>
+      <AuthProvider>
+        <body className={inter.className}>
+          {children}
+          <Toaster />
+        </body>
+      </AuthProvider>
     </html>
   );
 }
